97c2726cb5c0eb6c155fc7a981543090f56d651e
hye-jeong <50936796+HyeJeongHan@users.noreply.github.com>
2022-01-06 18:05:37 +0900

    Time: 617 ms (5.03%), Space: 42.5 MB (12.20%) - LeetHub

--- 1-two-sum/1-two-sum.kt
+++ 1-two-sum/1-two-sum.kt
@@ -1,14 +1,16 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         nums.forEachIndexed { firstIndex, prevNum ->
-        for ((secondIndex, nextNum) in nums.withIndex()) {
-            if (firstIndex == secondIndex) continue
-            if (prevNum + nextNum == target) {
-                return intArrayOf(firstIndex, secondIndex)
+            for ((secondIndex, nextNum) in nums.withIndex()) {
+                if (firstIndex == secondIndex) continue
+                if (prevNum + nextNum == target) {
+                    return intArrayOf(firstIndex, secondIndex)
+                }
             }
         }
-    }
     
         return intArrayOf()
     }
+    
+    // complement 보수 사용 
 }
